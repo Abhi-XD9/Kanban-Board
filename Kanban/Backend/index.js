94cbeeef3c3a8df34058a4b3402eb3b01fabb98d
Abhishek Anugu <158754433+Abhi-XD9@users.noreply.github.com>
2024-09-19 15:44:55 +0530

    Update index.js

--- Kanban/Backend/index.js
+++ Kanban/Backend/index.js
@@ -2,7 +2,7 @@ const express=require('express')
 const cors=require('cors')
 const app=express()
 const mysql=require('mysql2')
-const port= process.env.PORT || 9000
+const Port= process.env.PORT || 9000
 
 app.use(express.json());
 app.use(cors())
@@ -75,6 +75,6 @@ app.delete('/tasks/:id',(req,res)=>{
 })
 
 
-app.listen(port,()=>{
+app.listen(Port,()=>{
     console.log("Server Started")
 })
